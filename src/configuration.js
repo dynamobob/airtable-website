7bee7689bb97eba914b23d86b424eee561129f50
dynamobob <97067186+dynamobob@users.noreply.github.com>
2022-01-04 10:00:46 +1100

    Update configuration.js

--- src/configuration.js
+++ src/configuration.js
@@ -2,9 +2,9 @@ const config = {
   header: "Header",
   headerBackground: "#ff8ba7",
   bodyBackground: "#ffc6c7",
-  title: "Title",
-  subTitle: "Sub Title",
-  airtable: "https://airtable.com/embed/shrdi24ocwoEeXx6d?backgroundColor=red&viewControls=on",
+  title: "Bobs Hut",
+  subTitle: "Yes, this is bobs hut",
+  airtable: "https://airtable.com/embed/shr1I0RcCzlRO28ok?backgroundColor=red&viewControls=on",
   googleAnalytics: "UA-XXXXXXXX-X"
 }
 
